8d70b59d89c9b2f9930a95b376dbdac5d4e3fdb9
ketut <ketut@bps.go.id>
2020-06-22 21:06:27 +0800

    Update corona_bali.py

--- corona_bali.py
+++ corona_bali.py
@@ -4,10 +4,6 @@ from bs4 import BeautifulSoup
 from lxml import html
 import sys
 
-proxyku = {
-        'https':'https://icams:PASSWORD@proxy.bpsbali.id:3128/'
-        }
-
 headerku = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
         }
